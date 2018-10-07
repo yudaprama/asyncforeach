af9771200d912dac12302ca9134968d80fbade36
yuda <akuntestinguntukseto@gmail.com>
2018-10-07 14:57:02 +0700

    give function a name

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export default async (array, callback) => {
+export default asyncForEach async (array, callback) => {
   for (let index = 0; index < array.length; index++) {
     await callback(array[index], index, array)
   }
